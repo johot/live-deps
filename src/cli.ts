359c07191bb7108b471a9ca0dbe6351f6f48717a
Johan Otterud <johan.otterud@gmail.com>
2019-10-25 00:10:38 +0200

    This changes everything! No more syncing, just webpack modifications!

--- src/cli.ts
+++ src/cli.ts
@@ -1,32 +1,17 @@
 #!/usr/bin/env node
 "use strict";
 import program from "commander";
-import { startLivePackageSyncing, initializeLivePackageCra, startLivePackageCra } from "./index";
+import { initializeLivePackageCra } from "./index";
 import { version } from "./package.json";
 
 program.version(version);
 
 program
   .command("cra-init")
+  .alias("cra")
   .description("initialize live-package in create-react-app project")
   .action(() => {
     initializeLivePackageCra();
-  });
-
-program
-  .command("sync <packageDistFolder>")
-  .description("start syncing files between your app and the package dist folder")
-  .action(packageDistFolder => {
-    startLivePackageSyncing(packageDistFolder);
-  });
-
-program
-  .command("cra-start <packageDistFolder>")
-  .description(
-    "start syncing files and start your create-react-app using rewired, allowing cra to pickup changes to your package"
-  )
-  .action(packageDistFolder => {
-    startLivePackageCra(packageDistFolder);
   });
 
 program.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,27 @@
 import fs from "fs";
 import npmRun from "npm-run";
-import sdir from "sync-directory";
 import path from "path";
 import chalk from "chalk";
 import inquirer from "inquirer";
-import concurrently from "concurrently";
 import npmAddScript from "npm-add-script";
-import debounce from "debounce";
 
-export function devServer(configFunction: any, packageName: string) {
-  if (!packageName) throw Error("You didn't specify a package name for the live-package dev server!");
+export function modifyWebpack(config: any, packageName: string, packageFolder: string): any {
+  config.resolve.plugins = config.resolve.plugins.filter((p: any) => {
+    // Remove the ModuleScopePlugin or we will not be allowed so alias things outside the src folder
+    if (p && p.constructor && p.constructor.name && p.constructor.name.toString() === "ModuleScopePlugin") {
+      return false;
+    }
 
-  const overrideFn = function(proxy: any, allowedHost: any) {
-    const defaultConfig = configFunction(proxy, allowedHost);
+    return true;
+  });
 
-    //+(?!some_npm_module_name)
-    ///^(?!C:\/...\/src\/).+\/node_modules\//g
-    //const newRegex = defaultConfig.watchOptions.ignored.toString().replace("node_modules/", "node_modules/+(?!xxx)");
-
-    let ignoredFilesRegExp = defaultConfig.watchOptions.ignored;
-
-    // Append the package name so that it don't check node_modules folder *except* for your package
-    ignoredFilesRegExp = new RegExp(
-      ignoredFilesRegExp.source.replace("\\/node_modules\\/", "\\/node_modules\\/(?!" + escape(packageName) + ")"),
-      "g"
-    );
-
-    defaultConfig.watchOptions.ignored = ignoredFilesRegExp;
-
-    // We watch the node_modules folder for the package, it seems however webpack dev server only looks at the root folder
-    // so we will create a little "hack" by adding a new file to this folder with random content called ".live-reload"
-    // I guess (and hope) a better way exists, please let me know :) /johot
-    const currentContentBase = Array.isArray(defaultConfig.contentBase)
-      ? defaultConfig.contentBase
-      : [defaultConfig.contentBase];
-
-    defaultConfig.contentBase = [...currentContentBase, path.join(process.cwd(), "node_modules", packageName)];
-
-    return defaultConfig;
-  };
-
-  return overrideFn;
-}
-
-export function startLivePackageCra(packageDistFolder: string) {
-  concurrently(["react-app-rewired start", 'live-package sync "' + packageDistFolder + '"']);
+  config.resolve.alias = { ...config.resolve.alias, [packageName]: packageFolder };
+  return config;
 }
 
 export async function initializeLivePackageCra() {
-  const packageDistFolder = await getCommandLineAnswer(
-    "What is the path the your packages dist folder?",
+  const packageFolder = await getCommandLineAnswer(
+    "What is the path the your packages folder (where package.json is located)?",
     undefined,
     "input"
   );
@@ -59,14 +31,14 @@ export async function initializeLivePackageCra() {
   npmRun.execSync("npm i react-app-rewired --save-dev");
 
   console.log(chalk.magentaBright("- Creating react-app-rewired config file..."));
-  const packageName = getPackageNameFromDistFolder(packageDistFolder);
-  fs.writeFile("config-overrides.js", getConfigOverridesFile(packageName), err => {});
+  const packageName = getPackageNameFromPackageFolder(packageFolder);
+  fs.writeFile("config-overrides.js", getConfigOverridesFile(packageName, packageFolder), err => {});
 
   console.log(chalk.magentaBright("- Installing live-package from npm as a dev dependency..."));
   npmRun.execSync("npm i live-package --save-dev");
 
   console.log(chalk.magentaBright("- Adding live-package command to package.json scripts..."));
-  tryAddNpmScriptLine("start:lp", 'live-package cra-start "' + packageDistFolder + '"');
+  tryAddNpmScriptLine("start:lp", "react-app-rewired start");
   console.log(chalk.yellow("live-package initialized! ✔️"));
   console.log(
     chalk.magentaBright('Run command "') +
@@ -77,17 +49,17 @@ export async function initializeLivePackageCra() {
   );
 }
 
-function getConfigOverridesFile(packageDistFolder: string) {
+function getConfigOverridesFile(packageName: string, packageDistFolder: string) {
   return `/* config-overrides.js */
-    const livePackageDevServer = require("live-package").devServer;
-    
-    module.exports = {
-      // The function to use to create a webpack dev server configuration when running the development
-      // server with 'npm run start' or 'yarn start'.
-      // Example: set the dev server to use a specific certificate in https.
-      devServer: configFunction => livePackageDevServer(configFunction, "${packageDistFolder}")
-    };
-    `;
+const livePackage = require("live-package");
+
+module.exports = function override(config, env) {
+ 
+  livePackage.modifyWebpack(config, ${JSON.stringify(packageName)}, ${JSON.stringify(packageDistFolder)});
+
+  return config;
+};
+`;
 }
 
 export async function getCommandLineAnswer<T = string | boolean>(
@@ -113,70 +85,8 @@ export function tryAddNpmScriptLine(key: string, value: string) {
   } catch (ex) {}
 }
 
-/* config-overrides.js */
-// const livePackageDevServer = require("live-package").devServer;
-
-// module.exports = {
-//   // The function to use to create a webpack dev server configuration when running the development
-//   // server with 'npm run start' or 'yarn start'.
-//   // Example: set the dev server to use a specific certificate in https.
-//   devServer: livePackageDevServer
-// };
-
-function getPackageNameFromDistFolder(packageDistFolder: string) {
+function getPackageNameFromPackageFolder(packageDistFolder: string) {
   const packageJson = JSON.parse(fs.readFileSync(path.join(packageDistFolder, "package.json"), "utf8"));
   const packageName = packageJson.name;
   return packageName;
-}
-
-export function startLivePackageSyncing(packageDistFolder: string) {
-  const workingDirectory = process.cwd();
-  // Get the package name from the package json
-  const packageName = getPackageNameFromDistFolder(packageDistFolder);
-  const nodeModulesOutFolder = path.join(workingDirectory, "node_modules", packageName);
-
-  console.log(
-    chalk.magentaBright(
-      'Watching dist folder "' +
-        chalk.yellow(packageDistFolder) +
-        '" for changes, syncing to: "' +
-        chalk.yellow(nodeModulesOutFolder) +
-        '"'
-    )
-  );
-
-  // TODO: Find a better way via webpack config (if a way exists?)
-  const debouncedForceLiveReload = debounce(forceLiveReload, 200) as (
-    workingDirectory: string,
-    packageName: string
-  ) => void;
-
-  debouncedForceLiveReload(workingDirectory, packageName);
-
-  sdir(packageDistFolder, nodeModulesOutFolder, {
-    watch: true,
-    cb: (data: any) => {
-      console.log(
-        chalk.magentaBright("live-package detected change: ") +
-          chalk.yellow(data.path) +
-          " " +
-          chalk.redBright("syncing...")
-      );
-
-      // Debounced so we don't run it when we don't need to
-      debouncedForceLiveReload(workingDirectory, packageName);
-    }
-  });
-
-  // Forces webpack dev server to reload by changing the content of a dummy file in the root of the package
-  function forceLiveReload(workingDirectory: string, packageName: string) {
-    // TODO: Make smarter
-    const packageFolder = path.join(workingDirectory, "node_modules", packageName);
-    const triggerFile = path.join(packageFolder, ".live-package");
-
-    // Just change the file with random data
-    fs.writeFileSync(triggerFile, new Date().toString());
-
-    console.log(`${chalk.yellow("live-package")}: ${chalk.magentaBright("Reloading webpack dev server...")}`);
-  }
 }
